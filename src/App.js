499a9075b7228ec75c1fadc3f5858b933d53d6e5
Matthew Barty <100819562+BARTYlikesGitHub@users.noreply.github.com>
2022-07-09 00:39:34 +0930

    feat: imported MUI

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import * as React from 'react';
 import { Route, Routes, BrowserRouter as Router, Link } from "react-router-dom"
 import TodoComponent from './Pages/TodoList/TodoComponent.js'
 import Home from './Pages/Home.js'
 import About from './Pages/About.js'
 import ErrorPage from './Pages/ErrorPage.js'
+import { Tab, Tabs, Box } from '@mui/material';
+
 
 function App() {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+  const routes = ["/", "/about", "/todolist"]
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/todolist">Todo List</Link>
-            </li>
-          </ul>
-        </nav>
+        <Box>
+          <Tabs
+            onChange={handleChange}
+            value={value}
+            aria-label="Tabs where each tab needs to be selected manually"
+          >
+            <Tab label="Home Page" component={Link} to={routes[0]} />
+            <Tab label="About Page" component={Link} to={routes[1]} />
+            <Tab label="Todo List Page" component={Link} to={routes[2]} />
+          </Tabs>
+        </Box>
       </div>
       <div>
         <Routes>
